0ed921ad30b705d7397dc8ad4ad105e14eccae68
Lewis <bt20024@qmul.ac.uk>
2020-11-13 13:24:40 +0000

    None

--- WHYGIT1.R
+++ WHYGIT1.R
@@ -1,2 +1,20 @@
 #Why use GitHub? 
+<<<<<<< HEAD
 #A group presentation by:Lewis, Samar, Siv, Thomas, and Uppy.
+=======
+#A group presentation by:Lewis, Samar, Siv, Thomas, and Uppy. 
+
+x§x§
+
+# install and load tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+
+# plot a plot of data in ggplot2
+# this is the relationship of fuel efficiency (highway miles per gallon) vs
+# engine size in litres, colored by which type of drive train, 4 wheel drive (4),
+# front (f) or rear (r)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = drv))+
+  geom_smooth(mapping = aes(displ,hwy, color = drv))
+>>>>>>> a1e39470daaea7043f050caba59262b2c608965d
